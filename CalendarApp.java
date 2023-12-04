42958098066ea6f140b9c57bb8d07fc2a98cc975
KenWasHere31 <152862153+KenWasHere31@users.noreply.github.com>
2023-12-04 18:39:20 +0800

    Update CalendarApp.java

--- CalendarApp.java
+++ CalendarApp.java
@@ -1,3 +1,19 @@
+/**
+ * Project Calendar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * [Your Project Name] is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Project Calendar.  If not, see https://github.com/KenWasHere31/Projects/blob/main/LICENSE.
+ */
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
